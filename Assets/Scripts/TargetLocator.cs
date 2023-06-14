37ed29a23a8ed213f101a88a28a50c9dac36c49a
Json-Ram <jasonram778@gmail.com>
2023-06-14 03:20:16 -0700

    Create TargetLocator script, towers now target and follow enemy movement

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class TargetLocator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Transform weapon;
+    Transform target;
+
     void Start()
     {
-        
+        target = FindObjectOfType<EnemyMover>().transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        AimWeapon();
+    }
+
+    void AimWeapon()
+    {
+        weapon.LookAt(target);
     }
 }
